b7710045b67ea45afb21ab4fdba6b1ab75b2bdb5
Florian <alu.florian@gmail.com>
2021-08-27 12:02:42 +0200

    feat: Add findById, delete and edit method in HeroController

--- src/main/java/com/deerflow/danddmysql/characters/HeroController.java
+++ src/main/java/com/deerflow/danddmysql/characters/HeroController.java
@@ -1,10 +1,9 @@
 package com.deerflow.danddmysql.characters;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class HeroController {
@@ -20,5 +19,22 @@ public class HeroController {
     public int create(@RequestBody Hero hero) {
         Hero savedHero = repository.save(hero);
         return savedHero.getId();
+    }
+
+    @GetMapping("/characters/{id}")
+    public Optional<Hero> findById(@PathVariable(value = "id") Integer id) {
+        return repository.findById(id);
+    }
+
+    @DeleteMapping("/characters/{id}")
+    public void deleteOne(@PathVariable(value = "id") Integer id) {
+        repository.deleteById(id);
+    }
+
+    @PutMapping("/characters/{id}")
+    public Hero editOne(@PathVariable(value = "id") Integer id, @RequestBody Hero hero) {
+        Optional<Hero> maybeHero = repository.findById(id);
+        maybeHero.ifPresent(oldHero -> hero.setId(oldHero.getId()));
+        return repository.save(hero);
     }
 }
